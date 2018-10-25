e9bb31af3efeed4039dca649107b90229dc0a615
Lim Kimhuot <limkimhuot@gmail.com>
2018-10-25 13:53:31 +0700

    This is just another git message.

--- test.php
+++ test.php
@@ -1,7 +1,28 @@
 <?php
-function TestMuoy()
-{
-    return "YEs";
-}
+xdebug_break();
+ $i = 1;
 
-echo TestMuoy();
+ class RecursivePrinter
+ {
+
+   var $member;
+   var $float = 9.9;
+   function __construct() {
+     $this->member = array('key' => 'doom');
+
+     for($i = 0; $i < 10; $i++)
+       $this->member = array('key' => $this->member);
+   }
+
+   function recursivePrintNumber($limit, $cur = 0) {
+
+     print $cur."\n";
+     if($cur < $limit)
+       $this->recursivePrintNumber($limit, $cur+1);
+   }
+ }
+
+ $rs = new RecursivePrinter();
+ $rs->recursivePrintNumber(64); # Try setting a breakpoint on this line
+ throw new Exception();
+ print "finished";
